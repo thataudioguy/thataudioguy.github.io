87c4bfb4212e2e928d37ed346639c3f7a83bb808
Doug <dougmay17@gmail.com>
2018-01-17 12:22:01 -0700

    fade in changes to index

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    
+    setTimeout(function() {
+        $("#title").addClass("animated fadeInUp");
+        $("#buttons").addClass("animated fadeIn");
+        $("#title").removeClass("hidden");
+        $("#buttons").removeClass("hidden");
+    }, 400);
+    
+    
+});
