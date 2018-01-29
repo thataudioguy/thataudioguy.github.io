091ba06caf4eb21663b0cd66981db6eea9024674
Doug <dougmay17@gmail.com>
2018-01-29 16:57:07 -0700

    licensing page 1/29

--- js/licensing.js
+++ js/licensing.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
             $("#commercialButton").removeAttr("loaded");
             $("#commercialButton").removeClass("orangeFont");
             $("#cinematicButton").addClass("orangeFont");
-            $("#playlist").attr("src", "https://www.youtube.com/embed/videoseries?list=PL4HnzWbYV53dQmVOgcFbhZfJmoxmzIWs3"); 
+            $("#playlist").attr("src", "https://www.youtube.com/embed/videoseries?list=PLN5GL_fKa-4i0fnjXbUOEkx1UlNhYaHYa"); 
         };                
     });
 // end cinematic button.........................................................
@@ -17,7 +17,7 @@ $(document).ready(function(){
             $("#cinematicButton").removeAttr("loaded");
             $("#cinematicButton").removeClass("orangeFont");
             $("#commercialButton").addClass("orangeFont");
-            $("#playlist").attr("src", "https://www.youtube.com/embed/videoseries?list=PL4HnzWbYV53cKzd6mEzGd2Of5uKt3EQgg");  
+            $("#playlist").attr("src", "https://www.youtube.com/embed/videoseries?list=PLN5GL_fKa-4gj9QumV-X6cuc10wFBRmV2");  
         };   
     });
 // end commercial button.........................................................
